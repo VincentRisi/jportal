17d88b51b5c25e00c4afc1f12240000df301e839
Vincent Risi <vincent.risi@gmail.com>
2015-11-14 09:05:44 +0200

    crackled copyright

--- pyutils/fixup.py
+++ pyutils/fixup.py
@@ -2,13 +2,13 @@ import sys, glob, os
 
 copyright = '''\
 /// ------------------------------------------------------------------
-/// Copyright (c) from 1996 Vincent Risi 
-///                           
-/// All rights reserved. 
-/// This program and the accompanying materials are made available 
-/// under the terms of the Common Public License v1.0 
-/// which accompanies this distribution and is available at 
-/// http://www.eclipse.org/legal/cpl-v10.html 
+/// Copyright (c) from 1996 Vincent Risi
+/// 
+/// All rights reserved.
+/// This program and the accompanying materials are made available
+/// under the terms of the Common Public License v1.0
+/// which accompanies this distribution and is available at
+/// http://www.eclipse.org/legal/cpl-v10.html
 /// Contributors:
 ///    Vincent Risi
 /// ------------------------------------------------------------------
@@ -18,11 +18,11 @@ for file_name in glob.glob('/main/jportal/generators/vlab/crackle/*.java'):
     in_file = open(file_name, 'rt')
     lines = in_file.readlines()
     in_file.close()
-    next = False
+    skip = False
     for i, line in enumerate(lines):
         n = line.find('/// Copyright (c) from 1996 Vincent Risi')
         if n >= 0:
-            next = True
+            skip = True
             break
         n = line.find('package')
         if n >= 0:
@@ -30,9 +30,9 @@ for file_name in glob.glob('/main/jportal/generators/vlab/crackle/*.java'):
             for j, line in enumerate(copyright):
                 lines.insert(j, '%s\n' % line)
             break
-    if next == True:
+    if skip == True:
         continue
     print file_name
     out_file = open(file_name, 'w')
     out_file.writelines(lines)
-    break
+    ##break
