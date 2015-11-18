8d68a5cd0d6a0c73fda8d236e8ab0c83f4e7da18
Vincent Risi <vincent.risi@gmail.com>
2015-11-18 16:00:31 +0200

    new anydbMake additions

--- tools/anydbMake.py
+++ tools/anydbMake.py
@@ -8,13 +8,12 @@ import glob
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-B", "--buildPath",  dest="buildPath",  default=r"c:\sars\build")
-parser.add_option("-S", "--sourcePath", dest="sourcePath", default=r"c:\sars\code")
+parser.add_option("-B", "--buildPath",  dest="buildPath",  default="/vlab/build")
+parser.add_option("-S", "--sourcePath", dest="sourcePath", default="/vlab/source")
 parser.add_option("-b", "--build",      dest="build",      default=False, action="store_true", help='build all anyway')
-parser.add_option("-c", "--crackle",    dest="crackle",    default=r'c:\sars\tools\bbd\idl2\crackle.jar')
-parser.add_option("-j", "--jportal",    dest="jportal",    default=r'c:\sars\tools\bbd\anydb\jportal.jar')
-parser.add_option("-g", "--generate",   dest="generate",   default=False, action="store_true", help="generate CMake build only")
-parser.add_option("-l", "--local",      dest="local",      default=r"c:\sars")
+parser.add_option("-c", "--crackle",    dest="crackle",    default='crackle.jar')
+parser.add_option("-j", "--jportal",    dest="jportal",    default='jportal.jar')
+parser.add_option("-l", "--local",      dest="local",      default="/vlab")
 parser.add_option("-r", "--root",       dest="root",       default="")
 parser.add_option("-s", "--skip",       dest="skip",       default="", help=':CSIdl2Code:... colon switch list')
 parser.add_option("-v", "--verbose",    dest="verbose",    default=False, action="store_true", help="verbose")
@@ -77,7 +76,6 @@ class Project(object): pass
 class Switch(object): pass
 class Source(object): pass
 class Target(object): pass
-class CMake(object): pass
 
 def lastmod(name):
   if os.path.exists(name) == True:
@@ -131,7 +129,7 @@ def makeSwitches(project):
       continue
     if skips.find(':%s:' % switch.name[:-9]) >= 0:
       continue 
-    if switch.name[0:6] == 'PopUbi':
+    if switch.name[0:3] == 'Pop':
       switchName = "crackle"
     else:  
       switchName = "jportal"
@@ -182,91 +180,6 @@ def make_ib_files(pathlist):
       ibFiles.append(file)  
   return ibFiles
             
-def generate(project):
-  cmake = CMake() 
-  cmake.module = switches['idlModule']
-  cmake.target = switches['idlTarget']
-  cmake.idlBusLogicPath = switches['idlBusLogicPath'] 
-  cmake.include = switches['cmakeInclude']
-  cmake.source = switches['cmakeSource'] 
-  cmake.build = switches['cmakeBuild']
-  
-  def fileparts(filename):
-    head, tail = os.path.split(filename)
-    name, ext = os.path.splitext(tail)
-    return head, name, ext
-  
-  def cmake_fix(filename):
-    if filename.find(cmake.source) == 0:
-      result = '${CMAKE_SOURCE_DIR}%s' % (filename[len(cmake.source)-1:]).replace('\\','/')
-    elif filename.find(cmake.build) == 0:
-      result = '${CMAKE_BINARY_DIR}%s' % (filename[len(cmake.source)-1:]).replace('\\','/')
-    else:
-      result = filename.replace('\\','/')  
-    return result
-  
-  def add_typed_targets(sql_targets, ii_files, filename):
-    head, _, ext = fileparts(filename)
-    parts = head.split('\\')
-    key = parts[-1].upper()
-    fixed_name = cmake_fix(filename)
-    if not key in sql_targets: sql_targets[key] = []
-    sql_targets[key].append(fixed_name)
-    if ext == '.ii':
-      ii_files.append(fixed_name)
-      
-  cmake.name = fileparts(project.name)[1].upper()
-  lines = []
-  sql_targets = {}
-  idl_targets = {}
-  ii_files = []
-  lines.append('SET(%s_PROJECT_FILE' % (cmake.name))
-  lines.append('  %s' % (cmake_fix(project.name)))
-  lines.append('  )')
-  lines.append('')
-  lines.append('SET(%s_IM_FILE' % (cmake.name))
-  lines.append('  %s' % (cmake_fix(cmake.module)))
-  lines.append('  )')
-  lines.append('')
-  lines.append('SET(%s_SI_FILES' % (cmake.name))
-  for source in project.sources:
-    lines.append('  %s' % (cmake_fix(source.name)))
-    for target in source.targets:
-      add_typed_targets(sql_targets, ii_files, target.name)
-  lines.append('  )')
-  lines.append('')
-  keys = sql_targets.keys()
-  keys.sort()
-  for key in keys: 
-    lines.append('SET(%s_SQL_%s_OUTPUT' % (cmake.name, key))
-    for filename in sql_targets[key]:
-      lines.append('  %s' % (cmake_fix(filename)))
-    lines.append('  )')
-    lines.append('')
-  lines.append('SET(%s_II_FILES' % (cmake.name))
-  for file in ii_files:
-    lines.append('  %s' % (file))
-  lines.append('  )')
-  lines.append('')
-  ib_files = make_ib_files(cmake.idlBusLogicPath)
-  lines.append('SET(%s_IB_FILES' % (cmake.name))
-  for file in ib_files:
-    lines.append('  %s' % (file))
-  lines.append('  )')
-  lines.append('')
-  source = project.idl
-  for target in source.targets:
-    add_typed_targets(idl_targets, ii_files, target.name)
-  keys = idl_targets.keys()
-  keys.sort()
-  for key in keys: 
-    lines.append('SET(%s_IDL_%s_OUTPUT' % (cmake.name, key))
-    for filename in idl_targets[key]:
-      lines.append('  %s' % (cmake_fix(filename)))
-    lines.append('  )')
-    lines.append('')
-  for line in lines: print line
-
 class Handler(xml.sax.ContentHandler):
   def startElement(self, name, attrs):
     if name == 'Project':
@@ -299,107 +212,146 @@ class Handler(xml.sax.ContentHandler):
       target.name = fixname(attrs.getValue('Name'))
       target.lastmod = lastmod(target.name)
       self.source.targets.append(target)
+args = {}
 
-a = xml.sax.make_parser()
-handler = Handler()
-a.setContentHandler(handler)
-a.parse(sourceFile)
-project = handler.project
-makeSwitches(project)
-if options.generate == True:
-  generate(project)
-else:
-  clean(project)
-  projmod = lastmod(sourceFile)
-  jportalJarMod = lastmod(jportalJar)
-  crackleJarMod = lastmod(crackleJar)
-  sourceList = []
-  reasons = {}
-  iiFiles = []
-  ibFiles = [] 
-  for source in project.sources:
-    if source.noTargets == 0:
-      reasons[source.name] = 'no targets'
-      log('%s %s' % (source.name, reasons[source.name]))
-      sourceList.append(source.name)
+def remove_comment(line):
+  p = line.find('#')
+  if p >= 0:
+    line = line[:p]
+  return line
+
+def expand(line):
+  result = ''
+  while True:
+    s = line.find('${')
+    if s < 0:
+      result += line
+      break
+    result += line[:s]
+    line = line[s+2:]
+    e = line.find('}')
+    if e < 0:
+      result += line
+      break
+    arg = line[:e]
+    line = line[e+1:]
+    if args.has_key(arg):
+      result += args[arg]
+  return result
+
+def parse(sourceFile):
+  ifile = open(sourceFile, 'r')
+  lines = ifile.readlines()
+  ifile.close()
+  for line in lines:
+    line=expand(remove_comment(line.strip()))
+    if len(line) == 0:
       continue
-    compile=False  
-    for target in source.targets:
-      if source.lastmod > target.lastmod:
-        reasons[source.name] = 'source newer than target %s' % (target.name)
-        compile=True
-        break
-      if jportalJarMod > target.lastmod:
-        reasons[source.name] = 'jar newer than target %s' % (target.name)
-        compile=True
-        break
-    if compile == True:
-      log('%s %s' % (source.name, reasons[source.name]))
-      sourceList.append(source.name)
-      for target in source.targets:
-        if os.path.exists(target.name) == True:
-          print 'removing %s' % (target.name)
-          os.remove(target.name)
-    else:
-      log(source.name, 'up to date')
-      for target in  source.targets:
-        head, tail = os.path.split(target.name)
-        root, ext = os.path.splitext(tail)
-        if ext == '.ii':
-          iiFile = Source()
-          iiFile.name = fixname(target.name)
-          iiFile.lastmod = lastmod(iiFile.name)
-          iiFiles.append(iiFile)
-      
-  if len(sourceList) > 0:
-    fd, fname = tempfile.mkstemp('.~tmp')
-    for source in sourceList:
-      if len(reasons[source]) > 0:
-        print '%s --- %s' % (source, reasons[source])
-      os.write(fd, '%s\n' % (source))
-    os.close(fd)
-    iiFiles = jportal('-f %s' % (fname), switches['jportal'], iiFiles)
-    os.remove(fname)
-  
-  if 'idlModule' in switches and 'idlTarget' in switches:
-    compile = False
-    idlTarget = Source()
-    idlTarget.name = fixname(switches['idlTarget'])
-    idlTarget.lastmod = lastmod(idlTarget.name)
-    idlModule = Source()
-    idlModule.name = fixname(switches['idlModule'])
-    idlModule.lastmod = lastmod(idlModule.name)
-    if idlModule.lastmod > idlTarget.lastmod:
-      compile = True
-    if crackleJarMod > idlTarget.lastmod:
-      compile = True
-    if 'idlBusLogicPath' in switches:
-      ibFileNames = make_ib_files(switches['idlBusLogicPath'])
-      for name in ibFileNames:
-        ibFile = Source()
-        ibFile.name = fixname(name)
-        ibFile.lastmod = lastmod(ibFile.name)
-        ibFiles.append(ibFile)
-        if ibFile.lastmod > idlTarget.lastmod:
-          compile = True
-    for iiFile in iiFiles:
-      if iiFile.lastmod > idlTarget.lastmod:
-        compile = True
-        
+    fields=line.split('=')
+    if len(fields) == 2:
+      args[fields[0]] = fields[1]
+    print line
+  return None      
+
+_, ext = os.path.splitext(sourceFile)
+if ext == '.prj':
+  a = xml.sax.make_parser()
+  handler = Handler()
+  a.setContentHandler(handler)
+  a.parse(sourceFile)
+  project = handler.project
+  makeSwitches(project)
+else:
+  project = parse(sourceFile)
+  exit(0)
+clean(project)
+projmod = lastmod(sourceFile)
+jportalJarMod = lastmod(jportalJar)
+crackleJarMod = lastmod(crackleJar)
+sourceList = []
+reasons = {}
+iiFiles = []
+ibFiles = [] 
+for source in project.sources:
+  if source.noTargets == 0:
+    reasons[source.name] = 'no targets'
+    log('%s %s' % (source.name, reasons[source.name]))
+    sourceList.append(source.name)
+    continue
+  compile=False  
+  for target in source.targets:
+    if source.lastmod > target.lastmod:
+      reasons[source.name] = 'source newer than target %s' % (target.name)
+      compile=True
+      break
+    if jportalJarMod > target.lastmod:
+      reasons[source.name] = 'jar newer than target %s' % (target.name)
+      compile=True
+      break
   if compile == True:
-    outfile = open(idlTarget.name, 'wt')
-    ifile = open(idlModule.name, 'rt')
+    log('%s %s' % (source.name, reasons[source.name]))
+    sourceList.append(source.name)
+    for target in source.targets:
+      if os.path.exists(target.name) == True:
+        print 'removing %s' % (target.name)
+        os.remove(target.name)
+  else:
+    log(source.name, 'up to date')
+    for target in  source.targets:
+      head, tail = os.path.split(target.name)
+      root, ext = os.path.splitext(tail)
+      if ext == '.ii':
+        iiFile = Source()
+        iiFile.name = fixname(target.name)
+        iiFile.lastmod = lastmod(iiFile.name)
+        iiFiles.append(iiFile)
+if len(sourceList) > 0:
+  fd, fname = tempfile.mkstemp('.~tmp')
+  for source in sourceList:
+    if len(reasons[source]) > 0:
+      print '%s --- %s' % (source, reasons[source])
+    os.write(fd, '%s\n' % (source))
+  os.close(fd)
+  iiFiles = jportal('-f %s' % (fname), switches['jportal'], iiFiles)
+  os.remove(fname)
+if 'idlModule' in switches and 'idlTarget' in switches:
+  compile = False
+  idlTarget = Source()
+  idlTarget.name = fixname(switches['idlTarget'])
+  idlTarget.lastmod = lastmod(idlTarget.name)
+  idlModule = Source()
+  idlModule.name = fixname(switches['idlModule'])
+  idlModule.lastmod = lastmod(idlModule.name)
+  if idlModule.lastmod > idlTarget.lastmod:
+    compile = True
+  if crackleJarMod > idlTarget.lastmod:
+    compile = True
+  if 'idlBusLogicPath' in switches:
+    ibFileNames = make_ib_files(switches['idlBusLogicPath'])
+    for name in ibFileNames:
+      ibFile = Source()
+      ibFile.name = fixname(name)
+      ibFile.lastmod = lastmod(ibFile.name)
+      ibFiles.append(ibFile)
+      if ibFile.lastmod > idlTarget.lastmod:
+        compile = True
+  for iiFile in iiFiles:
+    if iiFile.lastmod > idlTarget.lastmod:
+      compile = True
+if compile == True:
+  outfile = open(idlTarget.name, 'wt')
+  ifile = open(idlModule.name, 'rt')
+  outfile.write(ifile.read())
+  ifile.close() 
+  for iiFile in iiFiles:
+    outfile.write('// *** %s\n' % (iiFile.name))
+    ifile = open(iiFile.name, 'rt')
     outfile.write(ifile.read())
-    ifile.close() 
-    for iiFile in iiFiles:
-      outfile.write('// *** %s\n' % (iiFile.name))
-      ifile = open(iiFile.name, 'rt')
-      outfile.write(ifile.read())
-      ifile.close()
-    for ibFile in ibFiles:
-      outfile.write('// *** %s\n' % (ibFile.name))
-      ifile = open(ibFile.name, 'rt')
-      outfile.write(ifile.read())
-      ifile.close()
-    outfile.close()
-    crackle(idlTarget.name, switches['crackle'])
+    ifile.close()
+  for ibFile in ibFiles:
+    outfile.write('// *** %s\n' % (ibFile.name))
+    ifile = open(ibFile.name, 'rt')
+    outfile.write(ifile.read())
+    ifile.close()
+  outfile.close()
+  crackle(idlTarget.name, switches['crackle'])
