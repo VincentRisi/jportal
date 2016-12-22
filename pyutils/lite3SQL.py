2e286ce1ac280c0f4dd2c241f257c0bbce06e7e8
vince <vince@bbd.co.za>
2016-12-22 12:56:09 +0200

    adding sqlite

--- pyutils/lite3SQL.py
+++ pyutils/lite3SQL.py
@@ -1,5 +1,6 @@
 import sqlite3
 import optparse
+import time
 
 def get_args():
   parser = optparse.OptionParser()
@@ -13,22 +14,59 @@ def get_args():
   return parser.parse_args()
 
 def execute(conn, cursor, buff):
+  global result
   buff = buff.replace(';', '').strip()
   if buff.lower() == 'commit':
     conn.commit()
   else:
     print '>> %s' % buff
-    cursor.execute(buff)
-    if cursor.rowcount == -1:
-      for row in cursor:
-        print row
-    else:
-      print 'rows affected %d' % (cursor.rowcount)
-    
+    tries = 5
+    while True:
+      try:
+        cursor.execute(buff)
+        if cursor.rowcount == -1:
+          for row in cursor:
+            print row
+        else:
+          print 'rows affected %d' % (cursor.rowcount)
+        break
+      except sqlite3.OperationalError as soe:
+        if soe == 'database is locked':
+          if tries > 0:
+            tries -= 1
+            time.sleep(0.13)
+            print '-- database is locked -- retry %d' % (5 - tries)
+            continue
+          print type(ex)       
+          print ex
+          result = 1
+          break
+      except Exception as ex:
+        print type(ex)       
+        print ex
+        result = 1
+        break
+
+result = 0
 
 def main():
+  global result
   options, args = get_args()
-  conn = sqlite3.connect(options.db_file)
+  tries = 5
+  while True:
+    try:
+      conn = sqlite3.connect(options.db_file)
+      break
+    except Exception as ex:
+      if tries > 0:
+        tries -= 1
+        time.sleep(0.13)
+        print '-- database is locked -- retry %d' % (5 - tries)
+        continue
+      print type(ex)       
+      print ex
+      result = 1
+      return result
   cursor = conn.cursor()
   for fileName in args:
     print 'Running script %s' % (fileName)
@@ -44,17 +82,18 @@ def main():
         execute(conn, cursor, buff)
         buff = ''
     if len(buff) > 0:
-      try:
-        execute(conn, cursor, buff)
-      except Exception as inst:
-        print type(inst)
-        print inst.args
-      else:
-        continue
+      execute(conn, cursor, buff)
     sql.close()
   cursor.close()
   conn.close()
 
 if __name__ == '__main__':
-  exit(main())
+  try:
+    main()
+  except Exception as ex:
+    print type(ex)       
+    print ex.args
+    print ex
+    result = 1
+  exit(result)
   
