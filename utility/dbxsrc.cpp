091770ec68aafd5d983ac4a7fb3e4111bbc7526e
Vincent Risi <vincent.risi@gmail.com>
2017-10-22 19:41:37 +0200

    add stat to check if file

--- utility/dbxsrc.cpp
+++ utility/dbxsrc.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <malloc.h>
+#include <sys/stat.h>
 #include "addlist.h"
 
 #define SIZE 64*1024
@@ -56,8 +57,18 @@ int main(int argc, char* argv[])
   {
     if (strcmp(sources[i], fpath) != 0)
     {
+      struct stat path_stat;
+      stat(fpath, &path_stat);
+      //printf("dir:%d chr:%d blk:%d reg:%d\n"
+      //  , S_ISDIR(path_stat.st_mode) 
+      //  , S_ISCHR(path_stat.st_mode) 
+      //  , S_ISBLK(path_stat.st_mode) 
+      //  , S_ISREG(path_stat.st_mode) 
+      //  );
+      if (S_ISDIR(path_stat.st_mode) == 0 && S_ISREG(path_stat.st_mode == 0))
+        continue;
       strncpy(fpath, sources[i], sizeof(fpath) - 1);
-      printf("%s\n", fpath);
+      printf("%s\n", fpath);    
     }
   }
 
